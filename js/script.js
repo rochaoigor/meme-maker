3ed684f2608a2f7aea2c6a76943352df70832ff7
rochaoigor <igorsantooos@gmail.com>
2022-11-18 17:54:54 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+function enablePhotoUpload() {
+    const imageInput = document.querySelector('#image-input');
+    imageInput.addEventListener('change', function() {
+        const reader = new FileReader();
+        reader.addEventListener("load", () =>{
+            const uploadImage = reader.result
+          changeMemePicture(uploadImage);
+        })
+        reader.readAsDataURL(this.files[0])
+    })
+}
+async function mapImageList() {
+ const memeObject = [
+    {
+        "name": "chapolin",
+        "path": "img/chapolin.jpg"
+
+    },
+    {
+        "name": "funny-cat1",
+        "path": "img/funny-cat1.png"
+
+    },
+
+      {
+        "name": "funny-cat2",
+        "path": "img/funny-cat2.png"
+
+    },
+
+    {
+        "name": "Side_Eyeing_Chloe",
+        "path": "img/Side_Eyeing_Chloe.jpg"
+
+    }
+ ]
+return memeObject
+}
+
+async function createGallery(image) {
+    const memeSelector = document.querySelector('#meme-list');
+    image.forEach(picture => {
+        let newOption = document.createElement('option');
+        newOption.text = picture.name.toUpperCase();
+        newOption.value = picture.path
+        memeSelector.appendChild(newOption);
+    })
+}
+
+async function changeMemePicture(photo) {
+    let displayImage = document.querySelector('#display-image');
+    displayImage.style.backgroundImage = `url('${photo}')`
+}
+async function init() {
+  const memeImageList = await mapImageList();
+  enablePhotoUpload();
+  await createGallery(memeImageList);
+  await changeMemePicture(memeImageList[0].path)
+}
+
+init();
